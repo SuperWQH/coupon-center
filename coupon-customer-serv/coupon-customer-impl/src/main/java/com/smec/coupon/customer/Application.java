e07406d7067f404dda7c4f42d922fc5dcb64cf30
weiqiheng <759318304@qq.com>
2023-04-24 14:47:21 +0800

    第一次修改

--- coupon-customer-serv/coupon-customer-impl/src/main/java/com/smec/coupon/customer/Application.java
+++ coupon-customer-serv/coupon-customer-impl/src/main/java/com/smec/coupon/customer/Application.java
@@ -24,5 +24,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
     }
 }
